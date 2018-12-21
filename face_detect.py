41e331005f7c9fd81a4d1d2fda988581c8e44aa2
Álvaro Emparán <alvaro.emparan@gmail.com>
2018-12-21 15:04:33 -0300

    add fotos

--- face_detect.py
+++ face_detect.py
@@ -14,11 +14,11 @@ gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 # Detect faces in the image
 faces = faceCascade.detectMultiScale(
-    gray,
-    scaleFactor=1.1,
-    minNeighbors=5,
-    minSize=(30, 30),
-    flags = cv2.cv.CV_HAAR_SCALE_IMAGE
+   gray,
+   scaleFactor=1.1,
+   minNeighbors=5,
+   minSize=(30, 30),
+   flags = cv2.CASCADE_SCALE_IMAGE 
 )
 
 print("Found {0} faces!".format(len(faces)))
